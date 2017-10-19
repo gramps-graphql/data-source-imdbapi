379d13f16f2271cdcb9568ff0700e627dd4a83cf
Jason Lengstorf <jason@lengstorf.com>
2017-10-19 13:09:23 -0500

    feat(Model): add data access methods

--- src/model.js
+++ src/model.js
@@ -5,21 +5,90 @@ import { GraphQLModel, GrampsError } from '@gramps/gramps-express';
  * https://ibm.biz/graphql-data-source-model
  */
 
-// TODO: change `YourDataSourceModel` to a descriptive name
-export default class YourDataSourceModel extends GraphQLModel {
+const makeUrlSafe = str => encodeURIComponent(str).replace('%20', '+');
+const getQueryString = args =>
+  Object.keys(args)
+    .map(key => (args[key] ? `${key}=${makeUrlSafe(args[key])}` : false))
+    // Remove any arguments that were falsy.
+    .filter(pair => pair !== false)
+    // Turn the array into a query string.
+    .join('&');
+
+export default class IMDBAPIModel extends GraphQLModel {
   /**
-   * Loads a thing by its ID
-   * @param  {String}  id  the ID of the thing to load
-   * @return {Promise}     resolves with the loaded user data
+   * Searches for a movie by its title and (optionally) its release year.
+   *
+   * @see http://www.theimdbapi.org/
+   * @param  {String}  args.title  movie title to search for
+   * @param  {String?} args.year   year the movie was released
+   * @return {Promise}             resolves with movie(s) matching the search
    */
-  getById(id) {
-    return this.connector.get(`/data/${id}`).catch(res =>
-      this.throwError(res, {
-        description: 'This is an example call. Add your own!',
-        docsLink:
-          'https://gramps-graphql.github.io/gramps-express/data-source/tutorial/',
-      }),
-    );
+  searchMoviesByTitle(args) {
+    return this.connector
+      .get(`/find/movie?${getQueryString(args)}`)
+      .catch(res =>
+        this.throwError(res, {
+          description: 'Unable to search movies',
+          docsLink: 'https://github.com/gramps-express/data-source-imdbapi',
+        }),
+      );
+  }
+
+  /**
+   * Searches for a person by their name.
+   *
+   * A quirk of the IMDB API is that it seems to only return a single result for
+   * searches. Bear that in mind when making searches.
+   *
+   * @see http://www.theimdbapi.org/
+   * @param  {String}  name  name to search for
+   * @return {Promise}       resolves with person matching the search
+   */
+  searchPersonByName(name) {
+    return this.connector
+      .get(`/find/person?${getQueryString({ name })}`)
+      .catch(res =>
+        this.throwError(res, {
+          description: 'Unable to search people',
+          docsLink: 'https://github.com/gramps-express/data-source-imdbapi',
+        }),
+      );
+  }
+
+  /**
+   * Retrieves a movie by its ID.
+   *
+   * @see http://www.theimdbapi.org/
+   * @param  {String}  movieId  the IMDB movie ID
+   * @return {Promise}          resolves with movie matching the ID
+   */
+  getMovieById(movieId) {
+    return this.connector
+      .get(`/movie?${getQueryString({ movie_id: movieId })}`)
+      .catch(res =>
+        this.throwError(res, {
+          description: 'Unable to get movie by ID',
+          docsLink: 'https://github.com/gramps-express/data-source-imdbapi',
+        }),
+      );
+  }
+
+  /**
+   * Retrieves a person by their IMDB ID.
+   *
+   * @see http://www.theimdbapi.org/
+   * @param  {String}  personId  the IMDB person ID
+   * @return {Promise}           resolves with person matching the ID
+   */
+  getPersonById(personId) {
+    return this.connector
+      .get(`/person?${getQueryString({ person_id: personId })}`)
+      .catch(res =>
+        this.throwError(res, {
+          description: 'Unable to get person by ID',
+          docsLink: 'https://github.com/gramps-express/data-source-imdbapi',
+        }),
+      );
   }
 
   /**
